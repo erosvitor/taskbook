e717a20aca2b7e9c872764199f78cd7448389ec4
erosvitor <erosvitor@yahoo.com.br>
2023-12-22 23:14:07 -0300

    Charset e Collate explicitos na criação do banco de dados

--- database/taskbook.sql
+++ database/taskbook.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE taskbook CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci;
+
 USE taskbook;
 
 CREATE TABLE tasks (
@@ -6,5 +8,4 @@ CREATE TABLE tasks (
   initial_date DATETIME NOT NULL,
   final_date DATETIME NOT NULL,
   PRIMARY KEY (id)
-);
-
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE utf8mb3_general_ci;
